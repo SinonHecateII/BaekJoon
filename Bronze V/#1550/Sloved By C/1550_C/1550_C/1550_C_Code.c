4d0494cdb8f142d1889042c0e56bb599b9f44dc5
SinonHecateII <sinonhecateii297@gmail.com>
2021-12-11 11:47:42 +0900

    BaekJoon Solve #1550 Using Python 2021-12-11

---
+++ Bronze V/#1550/Sloved By C/1550_C/1550_C/1550_C_Code.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf_s("%x", &num);
+
+    printf("%d", num);
+
+    return 0;
+}
---
+++ Bronze V/#1550/Solved By Python/1550_Py/1550_Py/_1550_Py.py
@@ -0,0 +1,19 @@
+X_num = input()
+
+D_num = 0
+temp = len(X_num) - 1
+
+for i in range(0, len(X_num)):
+    D_num = D_num + (int(X_num[temp], 16) * (16 ** i))
+    temp = temp - 1
+
+print(D_num)
+
+"""
+Shorter Code (After Solving it)
+Use Built-in Function
+
+code..
+print(int(input(), 16))
+
+"""
