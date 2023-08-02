fab8615858b3e1220d3ad6325b79e6a5ecd914f5
SinonHecateII <61343001+SinonHecateII@users.noreply.github.com>
2023-08-02 11:03:11 +0900

    2023-08-02 Silver 5 Class 2 #11651 Solved

--- Silver V/#11651/#11651.py
+++ Silver V/#11651/#11651.py
@@ -5,8 +5,9 @@ N = int(input())
 NumList = []
 for i in range(0, N):
     x, y = map(int, sys.stdin.readline().split())
-    NumList.append([x ,y])
+    NumList.append([x, y])
 
-NumList.sort()
+NumList = sorted(NumList, key = lambda x:x[1])
+
 for i in range(0, N):
     print(str(NumList[i][0]) + " " + str(NumList[i][1]))
